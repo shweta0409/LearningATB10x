67e68a9c213df6c0da69a3fc1dc2588237b8f450
win 10 <shwetapatil131101@gmail.com>
2025-01-21 12:03:46 +0530

    learning code

---
+++ src/ex_02_java_basic/Lab025_printf_arith.java
@@ -0,0 +1,35 @@
+package ex_02_java_basic;
+
+public class Lab025_printf_arith {
+    public static void main(String[] args) {
+        int a = 9*1;
+        int b = 9*2;
+        int c = 9*3;
+        int d = 9*4;
+        int e = 9*5;
+        int f = 9*6;
+        int g = 9*7;
+        int h = 9*8;
+        int i = 9*9;
+        int j = 9*10;
+        System.out.printf("%d",a);
+        System.out.println();
+        System.out.printf("%d",b);
+        System.out.println();
+        System.out.printf("%d",c);
+        System.out.println();
+        System.out.printf("%d",d);
+        System.out.println();
+        System.out.printf("%d",e);
+        System.out.println();
+        System.out.printf("%d",f);
+        System.out.println();
+        System.out.printf("%d",g);
+        System.out.println();
+        System.out.printf("%d",h);
+        System.out.println();
+        System.out.printf("%d",i);
+        System.out.println();
+        System.out.printf("%d",j);
+    }
+}
