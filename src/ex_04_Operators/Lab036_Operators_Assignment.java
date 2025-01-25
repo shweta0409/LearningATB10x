b78748b438f80e2ad0cc0809aaf6462fbc76e34d
win 10 <shwetapatil131101@gmail.com>
2025-01-25 22:36:33 +0530

    learning code

---
+++ src/ex_04_Operators/Lab036_Operators_Assignment.java
@@ -0,0 +1,19 @@
+package ex_04_Operators;
+
+public class Lab036_Operators_Assignment {
+
+        public static void main(String[] args) {
+            // Assignment operator
+
+            int age = 90;
+
+            // int -> data type
+            // age -> identifier
+            // 90 -> Literal
+            // = -> ? operator
+
+            // RHV to the LH
+
+        }
+    }
+
---
+++ src/ex_04_Operators/Lab055_interview.java
@@ -0,0 +1,7 @@
+package ex_04_Operators;
+
+public class Lab055_interview {
+    public static void main(String[] args) {
+        System.out.println('A' == 65); // == char -> ASCII - 65
+    }
+}
