242c4e525ba610d32c4ac4cbf606d126a1edd752
win 10 <shwetapatil131101@gmail.com>
2025-01-27 22:30:59 +0530

    learning code

---
+++ src/ex_14_Strings/Lab136_Interview.java
@@ -0,0 +1,9 @@
+package ex_14_Strings;
+
+public class Lab136_Interview {
+    public static void main(String[] args) {
+        String name = "shweta";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
---
+++ src/ex_14_Strings/Lab139_String_Functions.java
@@ -0,0 +1,7 @@
+package ex_14_Strings;
+
+public class Lab139_String_Functions {
+    public static void main(String[] args) {
+
+    }
+}
